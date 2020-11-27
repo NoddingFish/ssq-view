c7e76c07d154d37b89caefdbe0cb39ceb43f9900
徐然 <nlxuran@163.com>
2020-11-28 00:01:13 +0800

    first commit

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,20 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <el-row>
+    <el-button type="danger" v-html="'01'" circle></el-button>
+    <el-button type="danger" v-html="'12'"  circle></el-button>
+    <el-button type="danger" v-html="'18'"  circle></el-button>
+    <el-button type="danger" v-html="'19'"  circle></el-button>
+    <el-button type="danger" v-html="'27'"  circle></el-button>
+    <el-button type="danger" v-html="'30'"  circle></el-button>
+    <el-button type="primary" v-html="'02'"  circle></el-button>
+</el-row>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
+  name: 'Home'
 }
 </script>
