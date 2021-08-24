69ec71d25ac4764694aec8d61f547f5908d8fec3
打盹的鱼 <nlxuran@163.com>
2021-08-25 00:47:07 +0800

    修改导航

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,23 +1,5 @@
 <template>
   <div class="home">
-    <el-menu :default-active="activeIndex" class="el-menu-demo" mode="horizontal" background-color="#550000" @select="handleSelect">
-      <el-menu-item index="1">处理中心</el-menu-item>
-      <el-submenu index="2">
-        <template slot="title">我的工作台</template>
-        <el-menu-item index="2-1">选项1</el-menu-item>
-        <el-menu-item index="2-2">选项2</el-menu-item>
-        <el-menu-item index="2-3">选项3</el-menu-item>
-        <el-submenu index="2-4">
-          <template slot="title">选项4</template>
-          <el-menu-item index="2-4-1">选项1</el-menu-item>
-          <el-menu-item index="2-4-2">选项2</el-menu-item>
-          <el-menu-item index="2-4-3">选项3</el-menu-item>
-        </el-submenu>
-      </el-submenu>
-      <el-menu-item index="3" disabled>消息中心</el-menu-item>
-      <el-menu-item index="4"><a href="https://www.ele.me" target="_blank">订单管理</a></el-menu-item>
-    </el-menu>
-    <div class="line"></div>
     <el-menu
       :default-active="activeIndex2"
       class="el-menu-demo"
@@ -27,8 +9,8 @@
       text-color="#fff"
       active-text-color="#ffd04b">
       <el-menu-item index="1">处理中心</el-menu-item>
-      <el-submenu index="2">
-        <template slot="title">我的工作台</template>
+      <el-submenu index="2" style="position: absolute;right: 10px">
+        <template slot="title">打盹会儿</template>
         <el-menu-item index="2-1">选项1</el-menu-item>
         <el-menu-item index="2-2">选项2</el-menu-item>
         <el-menu-item index="2-3">选项3</el-menu-item>
@@ -39,8 +21,6 @@
           <el-menu-item index="2-4-3">选项3</el-menu-item>
         </el-submenu>
       </el-submenu>
-      <el-menu-item index="3" disabled>消息中心</el-menu-item>
-      <el-menu-item index="4"><a href="https://www.ele.me" target="_blank">订单管理</a></el-menu-item>
     </el-menu>
     <el-row>
       <el-button type="danger" v-html="'01'" circle></el-button>
